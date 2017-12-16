d57fecaf131b797050a06d8a0778a57956f958eb
Daniel Levenson <daniellevenson4@gmail.com>
2017-12-16 15:56:38 -0500

    changed petlist

--- FindAPet/client/src/components/PetList.jsx
+++ FindAPet/client/src/components/PetList.jsx
@@ -4,4 +4,14 @@ import {BrowserRouter as Router, Link, Redirect, Route} from 'react-router-dom';
 
 import Pet from './Pet'
 
+const PetList = (props) => {
+	return(
+		<div className="pet-list">
+			{props.myPets.map(pet => {
+				return <Pet key={pet.id} pet={pet} />
+			})}
+		</div>
+	)
+}
+
 export default PetList;
