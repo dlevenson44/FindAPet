77cdc3f83608d445e452db9b1cb147d6b02bc70a
Daniel Levenson <daniellevenson4@gmail.com>
2017-12-16 16:50:17 -0500

    set up SinglePet component

--- FindAPet/client/src/components/SinglePet.jsx
+++ FindAPet/client/src/components/SinglePet.jsx
@@ -1,53 +1,26 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
-class SinglePet extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			pet:{
-				name: props.pet ? props.pet.name : '',
-				post_type: props.pet ? props.pet.post_type : '',
-				animal: props.pet ? props.pet.animal : '',
-				breed: props.pet ? props.pet.breed : '',
-				age: props.pet ? props.pet.age : '',
-				picture: props.pet ? props.pet.picture : '',
-				description: props.pet ? props.pet.description : '',
-				foster_length: props.pet ? props.pet.foster_length : '',
-				id: props.pet ? props.pet.id : '',
-			},			
-		}
-		//this.fetchSinglePet = this.fetchSinglePet.bind(this)
-	}
-
-	// componentDidMount() {
-	// 	this.fetchSinglePet()
-	// }
-
-
-	// fetchSinglePet() {
-	// 	this.props.peList.map(pet => {
-
-	// 	})
-	// 	// fetch(`/pets/${id}`)
-	// 	fetch('/pets/:id')
-	// 	.then(res => res.json())
-	// 	.then(res => {
-	// 		console.log(res, 'this is res from singlepet')
-	// 		this.setState({
-	// 			pet: res
-	// 		})
-	// 	})	
-	// }
-
-	render() {
-		console.log(this, 'this is this from singlepet')
+const SinglePet = (props) => {
 	return(
-		<div>
-		<h1>hello</h1>
-		<p>this.state.pet.name</p>
+		<div className="pet-single">
+			<div className="inner">
+				<div className="img">
+					<img src={props.pet.picture} alt={props.pet.name} />
+				</div>
+				<div className="info">
+					<h4>{props.pet.name}</h4>
+					<h2>{props.pet.post_type}</h2>
+					<p>{props.foster_length}</p>
+					<h1>{props.pet.animal}</h1>
+					<h1>{props.pet.breed}</h1>
+					<h1>{props.pet.age}</h1>
+					<p>{props.pet.description}</p>
+				</div>
+			</div>
 		</div>
 		)
-	}
 }
+
 
 export default SinglePet;
