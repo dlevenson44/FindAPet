586169a4b685c0e5a2522762fb828a090eb068e9
Daniel Levenson <daniellevenson4@gmail.com>
2017-12-17 12:12:23 -0500

    moved has_attached_file line from user model to pet model

--- FindAPet/app/models/user.rb
+++ FindAPet/app/models/user.rb
@@ -5,8 +5,6 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_token :auth_token
   has_many :pets
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   #used to logout
   def invalidate_token
