fcac5d7a48aeefbe894dcbc7b65446c9901a411b
Daniel Levenson <daniellevenson4@gmail.com>
2017-12-18 09:51:47 -0500

    updated pet model for paperclip, removed picture column from seed data

--- FindAPet/app/models/pet.rb
+++ FindAPet/app/models/pet.rb
@@ -1,3 +1,7 @@
 class Pet < ApplicationRecord
+	#declares model has an attachment in the picture column
+	has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	#validates the content type being uploaded is an image
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 	belongs_to :user
 end
