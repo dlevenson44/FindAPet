94c67702c6e668b89ab2d5bbd3dd59ea73bf3335
Daniel Levenson <daniellevenson4@gmail.com>
2017-12-18 10:36:47 -0500

    removed picture mention pet component

--- FindAPet/client/src/components/Pet.jsx
+++ FindAPet/client/src/components/Pet.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {BrowserRouter as Router, Link, Redirect, Route} from 'react-router-dom';
 
+// <img src={props.pet.picture} />
+
 const Pet = (props) => {
 	return(
 		<div className="pet-inlist">
 			<h3>{props.pet.name}</h3>
-			<img src={props.pet.picture} />
+			
 			<Link to={`/pets/${props.pet.id}`}>See More</Link>
 		</div>
 	)
