d70b5f0907410830cc9f044b70eb5586503f4def
Daniel Levenson <daniellevenson4@gmail.com>
2017-12-19 14:57:57 -0500

    styled registration page

--- FindAPet/client/src/components/RegisterForm.jsx
+++ FindAPet/client/src/components/RegisterForm.jsx
@@ -28,19 +28,19 @@ class RegisterForm extends Component {
 
 	render(){
 		return(
-			<div className="form">
+			<div className="register-container">
 				<form onSubmit={(e) => this.props.handleRegisterSubmit(e, this.state)}>
-					<input type="text" name="username" placeholder="Username" value={this.state.username} onChange={this.handleChange} />
-					<input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} />
-					<input type="text" name="f_name" placeholder="First Name" value={this.state.f_name} onChange={this.handleChange} />
-					<input type="text" name="l_name" placeholder="Last Name" value={this.state.l_name} onChange={this.handleChange} />
-					<input type="text" name="address" placeholder="Address" value={this.state.address} onChange={this.handleChange} />
-					<input type="text" name="city" placeholder="City" value={this.state.city} onChange={this.handleChange} />
-					<input type="text" name="state" placeholder="State" value={this.state.state} onChange={this.handleChange} />
-					<input type="text" name="zip" placeholder="Zip" value={this.state.zip} onChange={this.handleChange} />
-					<input type="text" name="phone" placeholder="Phone Number" value={this.state.phone} onChange={this.handleChange} />
-					<input type="text" name="email" placeholder="Email Address" value={this.state.email} onChange={this.handleChange} />
-					<input type="submit" value="Create Account" />
+					<input className="register-box" type="text" name="username" placeholder="Username" value={this.state.username} onChange={this.handleChange} />
+					<input className="register-box" type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} />
+					<input className="register-box" type="text" name="f_name" placeholder="First Name" value={this.state.f_name} onChange={this.handleChange} />
+					<input className="register-box" type="text" name="l_name" placeholder="Last Name" value={this.state.l_name} onChange={this.handleChange} />
+					<input className="register-box" type="text" name="address" placeholder="Address" value={this.state.address} onChange={this.handleChange} />
+					<input className="register-box" type="text" name="city" placeholder="City" value={this.state.city} onChange={this.handleChange} />
+					<input className="register-box" type="text" name="state" placeholder="State" value={this.state.state} onChange={this.handleChange} />
+					<input className="register-box" type="text" name="zip" placeholder="Zip" value={this.state.zip} onChange={this.handleChange} />
+					<input className="register-box" type="text" name="phone" placeholder="Phone Number" value={this.state.phone} onChange={this.handleChange} />
+					<input className="register-box" type="text" name="email" placeholder="Email Address" value={this.state.email} onChange={this.handleChange} />
+					<button className="register-submit" type="submit">Register</button>
 				</form>
 			</div>
 			)
